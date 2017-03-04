0c5913e1869b0bf748b54c35fdde16c281608548
simplejia <353474225@qq.com>
2017-03-04 17:45:34 +0800

    tiny update

--- demo/filter/method.go
+++ demo/filter/method.go
@@ -8,7 +8,7 @@ import (
 func Method(w http.ResponseWriter, r *http.Request, p map[string]interface{}) bool {
 	method, ok := p["type"].(string)
 	if ok && strings.ToLower(r.Method) != strings.ToLower(method) {
-		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return false
 	}
 	return true
